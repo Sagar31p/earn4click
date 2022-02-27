089e6ef1c416f3c620bf55ffe842f5b446edb394
Sagar31p <99685466+Sagar31p@users.noreply.github.com>
2022-02-27 15:29:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,13 +34,13 @@ async def start(bot, message):
 async def start(bot, message):
     await message.reply(
         f"**Hello, {message.chat.first_name}!**\n\n"
-        "**If you send post which had Links, texts & images... Than I'll convert & replace all links with your links \nMessage me @kamdev07 For more help-**")
+        "**If you send post which had Links, texts & images... Than I'll convert & replace all links with your links \nMessage me @earn4clicks_in For more help-**")
 
 @bot.on_message(filters.command('support') & filters.private)
 async def start(bot, message):
     await message.reply(
         f"**Hey, {message.chat.first_name}!**\n\n"
-        "**please contact me on @kamdev07 or for more join @Doodstream_Admins**")
+        "**please contact me on @Earn4clicks_in or for more join @Earn4clicks_in)
     
 @bot.on_message(filters.text & filters.private)
 async def pdisk_uploader(bot, message):
@@ -73,7 +73,7 @@ async def pdisk_uploader(bot, message):
     
 async def pdisk_up(link):
     if ('mega' in link or 'google' in link or 'mdisk' in link or 'entertainvideo' in link or 'dood' in link or 'bit' in link ):
-        url = 'https://droplink.co/api'
+        url = 'https://pvidly.in/api'
         params = {'api': API_KEY, 'url': link}
     
         async with aiohttp.ClientSession() as session:
@@ -127,14 +127,8 @@ async def remove_username(new_List):
     return new_List
   
 async def addFooter(str):
-    footer = """\n__🔆Also available on Telegram in private channel Directly🔆__
-
-➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
-__#SavitaBhabhi #ComicVideo #Savita #Hindi #Sexy_voice #Kirtu #Savita_Bhabhi #Velamma #Crazydad #Momshelp #incest__
-
-🔅How to Download - https://youtu.be/7AvLkGx31_A \n
-📣 Provide By @"""+ CHANNEL + """
-__🔊For all  Direct Comics folder lifetime Membership msg me on @Kamdev07 or Join- @vip_comics__"""
+    footer = """\n__👉How to Watch/Download👈
+https://t.me/earn4click_tutorial"""
     return str + footer
         
 bot.run()
